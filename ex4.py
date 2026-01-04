2761904d66da3a62b14765646406e1da7e21e1a7
Timur Darii <timurdarii95@gmail.com>
2026-01-04 14:45:31 +0000

    add ex4

---
+++ ex4.py
@@ -0,0 +1,6 @@
+numar = int(input('Introduceti numar intre 1 si 10: '))
+
+while numar < 1 or numar > 10:
+    numar  = int (input('Gresit. Introduceti un numar intre 1 si 10: '))
+
+print ('Super!')
